f438bda81429cdd2670b26bf65e48bba0c17d18b
prongbang <anonymous>
2022-08-14 20:38:37 +0700

    Update UI

--- src/App.svelte
+++ src/App.svelte
@@ -4,26 +4,10 @@
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Wachasit.com</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/prongbang" target="_blank">Devไปวันๆ</a>, the Github
-  </p>
+  <h1>wachasit.com</h1>
 
   <p class="read-the-docs">
-    Click on the link to learn more
+    Click on the <a href="https://github.com/prongbang" target="_blank">Github</a> to learn more.
   </p>
 </main>
 
